06589fb571774fcac794b3c3c15833db7bad88c9
edwardyq67 <edwardyq67@gmail.com>
2024-03-16 17:51:43 -0500

    dsasddq

--- src/store/slice/proyecto.slice.tsx
+++ src/store/slice/proyecto.slice.tsx
@@ -1,8 +1,15 @@
-import { Action, createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Action, createSlice, PayloadAction, ThunkAction } from '@reduxjs/toolkit';
+ // Suponiendo que tengas definido AppThunk en tu aplicación
 import axios from 'axios';
-import { RootState } from '../ReturnType';
 import { setIsLoading } from './isLoading.slice';
+import { RootState } from '../ReturnType';
 
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
 interface ConocimientoItem {
     id: number;
     // Otros campos...
@@ -12,37 +19,28 @@ type ConocimientoState = ConocimientoItem[];
 
 const initialState: ConocimientoState = [];
 
-export const getConocimiento = createAsyncThunk(
-  'conocimiento/getConocimiento',
-  async (_, { dispatch }) => {
-    try {
-      dispatch(setIsLoading(true));
-      const response = await axios.get<ConocimientoItem[]>('http://soloportafolio-dev-bqsp.3.us-1.fl0.io/informacionProyect');
-      return response.data;
-    } catch (error) {
-      // Manejar errores aquí, por ejemplo, lanzar una excepción o devolver un valor predeterminado
-      console.error('Error al obtener el conocimiento:', error);
-      throw error;
-    } finally {
-      dispatch(setIsLoading(false));
-    }
-  }
-);
-
 export const conocimientoSlice = createSlice({
-  name: 'conocimiento',
-  initialState,
-  reducers: {
-    setConocimiento: (state, action: PayloadAction<ConocimientoState>) => {
-      state.splice(0, state.length, ...action.payload);
+    name: 'conocimiento',
+    initialState,
+    reducers: {
+        setConocimiento: (state, action: PayloadAction<ConocimientoState>) => {
+            state.splice(0, state.length, ...action.payload);
+        }
     }
-  },
-  extraReducers: (builder) => {
-    builder.addCase(getConocimiento.fulfilled, (state, action) => {
-      state.splice(0, state.length, ...action.payload);
-    });
-  }
 });
+
+export const getConocimiento = (): AppThunk => async (dispatch) => {
+    dispatch(setIsLoading(true));
+
+    try {
+        const response = await axios.get<ConocimientoItem[]>('http://soloportafolio-dev-bqsp.3.us-1.fl0.io/informacionProyect');
+        dispatch(setConocimiento(response.data));
+    } catch (error) {
+        // Manejar errores aquí
+    } finally {
+        dispatch(setIsLoading(false));
+    }
+}
 
 export const { setConocimiento } = conocimientoSlice.actions;
 
